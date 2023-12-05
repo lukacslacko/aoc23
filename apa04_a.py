3f35bbe2501370fc751585e51cc4869f61b35cd1
Laszlo Lukacs <lukacs@laszlos-mbp.lan>
2023-12-05 16:17:49 +0100

    apa day 4 a

---
+++ apa04_a.py
@@ -0,0 +1,4 @@
+print(sum(2**len(v[0].intersection(v[1]))//2 for v in
+[list(map(lambda s:set(int(x)for x in s.split()),l.
+split(":")[1].split("|")))for l in open("apa04input.txt"
+,"r").readlines()]))
